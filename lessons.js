efaaaa801ac658a5f669be2aea30701231195a14
BogdanPopescu0209 <59745574+BogdanPopescu0209@users.noreply.github.com>
2021-11-18 10:35:08 +0000

    Bootstrap, css and images added to display lessons

---
+++ lessons.js
@@ -0,0 +1,71 @@
+const lessons = [{
+    "title": "Math",
+    "location": "London",
+    "price": 100,
+    "quantity": 5,
+    "image": "images/math.png"
+},
+{
+    "title": "History",
+    "location": "York",
+    "price": 80,
+    "quantity": 5,
+    "image": "images/history.png"
+},
+{
+    "title": "Biology",
+    "location": "Manchester",
+    "price": 45,
+    "quantity": 5,
+    "image": "images/biology.png"
+},
+{
+    "title": "Geography",
+    "location": "Liverpool",
+    "price": 20,
+    "quantity": 5,
+    "image": "images/geography.png"
+},
+{
+    "title": "Physics",
+    "location": "Bristol",
+    "price": 150,
+    "quantity": 5,
+    "image": "images/physics.png"
+},
+{
+    "title": "Chemistry",
+    "location": "Leicester",
+    "price": 125,
+    "quantity": 5,
+    "image": "images/chemistry.png"
+},
+{
+    "title": "English",
+    "location": "Oxford",
+    "price": 200,
+    "quantity": 5,
+    "image": "images/english.png"
+},
+{
+    "title": "Art",
+    "location": "Liverpool",
+    "price": 15,
+    "quantity": 5,
+    "image": "images/art.png"
+},
+{
+    "title": "Science",
+    "location": "Cambridge",
+    "price": 250,
+    "quantity": 5,
+    "image": "images/science.png"
+},
+{
+    "title": "Drama",
+    "location": "Manchester",
+    "price": 75,
+    "quantity": 5,
+    "image": "images/drama.png"
+}
+]
